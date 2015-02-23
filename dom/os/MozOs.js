c5e9809317a715a8c42f3c89bb50d853034a94db
Jan Jongboom <janjongboom@gmail.com>
2015-02-23 15:21:20 +0100

    Add openFile, wip

--- dom/os/MozOs.js
+++ dom/os/MozOs.js
@@ -133,6 +133,18 @@ MozOs.prototype = {
         .then(res, rej);
     });
   },
+  
+  openFile: function(path, mode, options) {
+    return this.createPromise((res, rej) => {
+      OS.File.open(path, mode, options)
+        .then(file => {
+          let osFile = new MozOsFile(file);
+          osFile.init(this._window);
+          let wrappedOsFile = this._window.MozOsFile._create(this._window, osFile);
+          res(wrappedOsFile);
+        }, rej);
+    });
+  },
 
   exec: function(path, args) {
     return this.createPromise((res, rej) => {
@@ -161,6 +173,135 @@ MozOs.prototype = {
       catch (ex) {
         rej(ex);
       }
+    });
+  }
+};
+
+ /**
+ * ==============================================
+ * InputContext
+ * ==============================================
+ */
+function MozOsFile(file) {
+  this._file = file;
+}
+
+MozOsFile.prototype = {
+  __proto__: DOMRequestIpcHelper.prototype,
+
+  _file: null,
+  _window: null,
+
+  classID: Components.ID("{c42e6848-b8b1-45cf-a127-6df725cff842}"),
+
+  QueryInterface: XPCOMUtils.generateQI([
+    Ci.nsIObserver,
+    Ci.nsISupportsWeakReference
+  ]),
+  
+  wrapFileMethod: function(functionName) {
+    let self = this;
+    
+    return function() {
+      let args = arguments;
+      
+      return self.createPromise((res, rej) => {
+        self._file[functionName].apply(self._file, args).then((v) => {
+          dump(functionName + ' OK ' + v + '\n');
+          res(Cu.cloneInto(v, self._window));
+        }, err => {
+          dump(functionName + ' NOK ' + err + '\n');
+          rej(err);
+        });
+      });
+    };
+  },
+
+  init: function(win) {
+    this._window = win;
+    this._utils = win.QueryInterface(Ci.nsIInterfaceRequestor)
+                     .getInterface(Ci.nsIDOMWindowUtils);
+    this.initDOMRequestHelper(win, []);
+    
+    this.close = this.wrapFileMethod('close');
+    this.flush = this.wrapFileMethod('flush');
+    this.getPosition = this.wrapFileMethod('getPosition');
+    this.setDates = this.wrapFileMethod('setDates');
+  },
+
+  destroy: function() {
+    this.destroyDOMRequestHelper();
+
+    this._window = null;
+  },
+  
+  write: function(data, encoding) {
+    if (encoding === 'utf-8') {
+      let encoder = new TextEncoder();
+      return this.createPromise((res, rej) => {
+        this._file.write(encoder.encode(data))
+          .then(() => {
+            dump('Write OK\n');
+            res();
+          }, rej);
+      });
+    }
+    else if (encoding === 'binary') {
+      return this.createPromise((res, rej) => {
+        this._file.write(data)
+          .then(res, rej);
+      });
+    }
+    else {
+      throw 'Encoding "' + encoding + '" not supported. Try utf-8 or binary.';
+    }
+  },
+  
+  read: function(bytes, encoding) {
+    if (encoding === 'utf-8') {
+      return this.readUtf8(bytes);
+    }
+    else if (encoding === 'binary') {
+      return this.readBinary(bytes);
+    }
+    else {
+      return this.createPromise((res, rej) => {
+        rej('Encoding "' + encoding + '" not supported. Try utf-8 or binary.');
+      });
+    }
+  },
+
+  readBinary: function(bytes) {
+    return this.createPromise((res, rej) => {
+      this._file.read(bytes).then(array => {
+        res(Cu.cloneInto(array, this._window));
+      }).catch(err => rej(err));
+    });
+  },
+
+  readUtf8: function(bytes) {
+    let decoder = new TextDecoder();
+
+    return this.createPromise((res, rej) => {
+      this._file.read(bytes).then(array => {
+        res(decoder.decode(array));
+      }).catch(err => rej(err));
+    });
+  },
+  
+  stat: function() {
+    throw 'Yolo';
+  },
+  
+  setPosition: function(position, origin) {
+    dump('origin is ' + origin + '\n')
+    
+    origin = OS.File[origin];
+    
+    dump('origin is ' + origin + '\n')
+    
+    return this.createPromise((res, rej) => {
+      this._file.setPosition(position, origin).then(res, rej);
     });
   }
 };
--- dom/webidl/Os.webidl
+++ dom/webidl/Os.webidl
@@ -13,6 +13,8 @@ interface MozOs : EventTarget {
   Promise<void> removeFile(DOMString path);
   Promise<void> appendFile(DOMString path, (Uint8Array or DOMString) data, optional DOMString encoding = "binary");
   Promise<void> createDirectory(DOMString path, optional boolean ignoreExisting = true);
+  
+  Promise<MozOsFile> openFile(DOMString path, optional object mode, optional object options);
 };
 
 [JSImplementation="@mozilla.org/b2g-os-exec-response;1"]
@@ -20,4 +22,31 @@ interface MozOsExecResponse {
   readonly attribute long exitCode;
   readonly attribute DOMString stdout;
   readonly attribute DOMString stderr;
+};
+
+enum PositionOrigin {
+  "POS_START",
+  "POS_CUR",
+  "POS_END"
+};
+
+[JSImplementation="@mozilla.org/b2g-os-file;1"]
+interface MozOsFile {
+  Promise<void> close();
+  Promise<void> flush();
+  Promise<long> getPosition();
+  Promise<(DOMString or Uint8Array)> read(optional long bytes, optional DOMString encoding = "binary");
+  Promise<void> setDates(DOMTimeStamp accessDate, DOMTimeStamp modificationDate);
+  Promise<void> setPosition(long bytes, optional PositionOrigin origin = "POS_CUR");
+  Promise<MozOsFileInfo> stat();
+  Promise<long> write((Uint8Array or DOMString) data, optional DOMString encoding = "binary"); /* ArrayBufferView source, optional any options); */
+};
+
+[JSImplementation="@mozilla.org/b2g-os-fileinfo;1"]
+interface MozOsFileInfo {
+  readonly attribute boolean isDir;
+  readonly attribute boolean isSymLink;
+  readonly attribute long size;
+  readonly attribute DOMTimeStamp lastAccessDate;
+  readonly attribute DOMTimeStamp lastModificationDate;
 };
